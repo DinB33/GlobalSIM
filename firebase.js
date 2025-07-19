cee4dc682c99ee86e236e72b1a7c94b3f6a7375e
DinB33 <gomezburleson458@gmail.com>
2025-07-19 11:19:00 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,28 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
 
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-// Your Firebase config
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAyHjcP2xbiv6FqxGT8bWQI-RkttS1LekA",
   authDomain: "globalsim-80108.firebaseapp.com",
   projectId: "globalsim-80108",
-  storageBucket: "globalsim-80108.firebasestorage.app",
+  storageBucket: "globalsim-80108.appspot.com",
   messagingSenderId: "577978389706",
   appId: "1:577978389706:web:ffb307cf23a00237da1033",
   measurementId: "G-KJV1E1ZL71"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app);
-
-// Make auth functions available globally
-window.auth = auth;
-window.createUserWithEmailAndPassword = createUserWithEmailAndPassword;
-window.signInWithEmailAndPassword = signInWithEmailAndPassword;
-window.onAuthStateChanged = onAuthStateChanged;
-window.signOut = signOut;
